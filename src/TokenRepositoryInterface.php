f57c0b24d4fb5dfc0714cba3717c874145d3f090
azandrew-sidoine <azandrewdevelopper@gmail.com>
2023-07-27 13:30:55 +0000

    Added breaking changes to TokenRepositoryInterface contract

--- src/TokenRepositoryInterface.php
+++ src/TokenRepositoryInterface.php
@@ -38,4 +38,20 @@ interface TokenRepositoryInterface
      * Remove a token from tokens the collection.
      */
     public function deleteToken(string $sub): bool;
+
+    /**
+     * Query hashed token from password reset token string
+     * 
+     * @param string $token
+     * 
+     * @return HashedTokenInterface|null 
+     */
+    public function getByToken(string $token): ?HashedTokenInterface;
+
+    /**
+     * Prune the stale tokens from repository
+     * 
+     * @return void 
+     */
+    public function prune();
 }
